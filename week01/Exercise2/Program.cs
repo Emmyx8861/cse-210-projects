ce2bf638437408aa66fa704e9320bac15b9807f9
Adewemimo Emmanuel <eadewemimo@byupathway.edu>
2026-07-08 17:41:20 +0100

    grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your score ?");
+        string input = Console.ReadLine();
+        int score = int.Parse(input);
+
+        string letter = "";
+        if (score >= 90)
+        {
+           letter="A";
+        }
+        
+        else if (score >= 80)
+        {
+            letter="B";
+        }
+        else if (score >= 70)
+        {
+            letter="C";
+        }
+        else if (score >= 60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass the class.");
+        }
+        
+        if (letter=="A" && score % 10 >= 7)
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else if (letter=="F")
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+        else if (score % 10 >= 7)
+        {
+            Console.WriteLine($"Your grade is {letter}+");
+        }
+        else if (score % 10 < 3)
+        {
+            Console.WriteLine($"Your grade is {letter}-");
+        }
     }
 }
