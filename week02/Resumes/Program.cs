d43790bacf0bc00a2cea8c2d83473deb797f0ed7
Adewemimo Emmanuel <eadewemimo@byupathway.edu>
2026-07-10 20:53:04 +0100

    Resume Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Swish Sports";
+        job1._jobTitle="Market Lead Generation";
+        job1._startYear=2025;
+        job1._endYear=2027;
+
+        Job job2 = new Job();
+        job2._company= "Fon You";
+        job2._jobTitle="Custodian";
+        job2._startYear=2025;
+        job2._endYear=2027;
+
+        Resume resume = new Resume();
+        resume._name= "Adewemimo Emmanuel";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResume();
     }
 }
+
+
+
