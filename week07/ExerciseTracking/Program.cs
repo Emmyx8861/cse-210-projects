cc03b1cd15e2a2d1eee56eb26e9934c3eabd6217
Adewemimo Emmanuel <eadewemimo@byupathway.edu>
2026-08-03 00:59:07 +0100

    wk 7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+       Running run = new Running("03 Nov 2022", 30, 4.8);
+        Cycling cycle = new Cycling("04 Nov 2022", 45, 20.0);
+        Swimming swim = new Swimming("05 Nov 2022", 25, 20);
+
+        List<Activity> activities = new List<Activity> { run, cycle, swim };
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
-}
+    }
